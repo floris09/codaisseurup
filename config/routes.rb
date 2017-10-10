12074a314e2495b989097ddc6c3b22ea54b696da
floris09 <florismeininger@gmail.com>
2017-10-10 18:27:14 +0200

    Add profile

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   resources :events, except: [:destroy]
-  
+  resources :profiles, only: [:new, :edit, :create, :update]
+
+
 end
