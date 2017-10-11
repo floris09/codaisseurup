b3caa44a70b23de215ee0b367abe2baf1c5c05c4
floris09 <florismeininger@gmail.com>
2017-10-11 10:16:47 +0200

    Add categories to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    @categories = Category.all
+  end
+
+  def index
+    @events = Event.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :categories
   root to: 'pages#home'
 
   devise_for :users
+  resources :categories
   resources :users, only: [:show]
   resources :events, except: [:destroy]
   resources :profiles, only: [:new, :edit, :create, :update]
