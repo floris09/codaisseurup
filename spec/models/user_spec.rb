360acabc8d19fd8825f05c4ddcd5bb7b8cd6356b
floris09 <florismeininger@gmail.com>
2017-10-13 11:04:17 +0200

    Generate Registration model, add associations with User and Event, test associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,5 +17,15 @@ RSpec.describe User, type: :model do
     expect { user.destroy }.to change{ user.events.count }.by(-1)
   end
 
+  describe "association with booking" do
+    let(:guest_user) { create :user, email: "guest@user.com" }
+    let(:host_user) { create :user, email: "host@user.com" }
 
+    let!(:event) { create :event, user: host_user }
+    let!(:registration) { create :registration, event: event, user: guest_user }
+
+    it "has bookings" do
+      expect(guest_user.registered_events).to include(event)
+    end
+  end
 end
