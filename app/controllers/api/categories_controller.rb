a404e149273b39b8a930564626eae76f4ff68617
floris09 <florismeininger@gmail.com>
2017-10-16 16:41:51 +0200

    Add API events destroy and update

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -4,4 +4,33 @@ class Api::CategoriesController < ApplicationController
       categories: Category.all
       }.to_json
   end
+
+  def show
+    category = Category.find(params[:id])
+
+    render status: 200, json: {
+      category: category
+    }.to_json
+  end
+
+  def create
+    category = Category.new(category_params)
+
+    if category.save
+      render status: 201, json: {
+        message: "Category created",
+        category: category
+      }.to_json
+    else
+      render status: 422, json: {
+        errors: category.errors,
+      }.to_json
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
